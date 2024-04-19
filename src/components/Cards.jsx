7dae256a1c9d9e0d16eaf5ecb7ff0a91adf1a870
Dpinder <dpinder.mahay.786@gmail.com>
2024-04-19 22:57:25 +0530

    done

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,27 +1,51 @@
 import { FaRegFileAlt } from "react-icons/fa";
 import { LuDownload } from "react-icons/lu";
-export default function Cards() {
+import { IoIosClose } from "react-icons/io";
+import { motion } from "framer-motion";
+export default function Cards(prop) {
   return (
     <>
-      <div className=" relative w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white px-5 py-8 overflow-hidden">
+      <motion.div
+        drag
+        dragConstraints={prop.reference}
+        whileDrag={{ scale: 1.2 }}
+        dragElastic={0.2}
+        dragTransition={{
+          bounceStiffness:100,
+          bounceDamping:30
+        }}
+        className=" relative w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white px-5 py-8 overflow-hidden"
+      >
         <FaRegFileAlt />
         <p className="text-sm mt-5 leading-tight font-semibold">
-          Lorem ipsum, dolor sit amet consectetur adipisicing.
+          {prop.data.desc}
         </p>
         <div className=" footer absolute bottom-0 w-full   left-0">
-            <div className="flex items-center justify-between px-8 py-3 mb-5">
-            <h5>.4mb</h5>
+          <div className="flex items-center justify-between px-8 py-3 mb-5">
+            <h5>{prop.data.fileSize}</h5>
             <span className="w-7 h-7 bg-zinc-600 flex items-center rounded-full justify-center">
-            <LuDownload size='.7em' color="#FFF" />
+              {prop.data.close ? (
+                <IoIosClose />
+              ) : (
+                <LuDownload size=".7em" color="#FFF" />
+              )}
             </span>
+          </div>
+          {prop.data.tag.isOpen ? (
+            <div
+              className={`tag w-full py-4 ${
+                prop.data.tag.tagColor === "blue"
+                  ? "bg-blue-600"
+                  : "bg-green-600"
+              } flex items-center justify-center`}
+            >
+              <h3 className="text-sm font-semibold">
+                {prop.data.tag.tagTitle}
+              </h3>
             </div>
-            <div className="tag w-full py-4 bg-green-500 flex items-center justify-center">
-                <h3 className="text-sm font-semibold">Download Now</h3>
-            </div>
+          ) : null}
         </div>
-        footer
-      
-      </div>
+      </motion.div>
     </>
   );
 }
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,24 +1,42 @@
+import { useRef } from "react";
 import Cards from "./Cards";
 
 export default function Foreground() {
+    const ref =useRef()
     const data =[
         {
-        disc:"Lorem, ipsum dolor sit amet consectetur adipisicing.",
+        desc:"this is description text for card description.",
         fileSize:".9mb",
-        close:"true",
+        close: false,
         tag:{
-            isOpen:"true",tagTitle:"Download Now",tagColor:"green"
+            isOpen:true,tagTitle:"Download Now",tagColor:"green"
         }
-        }
+        },
+        {
+            desc:"this is description text for card description.",
+            fileSize:".9mb",
+            close: true,
+            tag:{
+                isOpen:false,tagTitle:"Download Now",tagColor:"green"
+            }
+            },
+            {
+                desc:"this is description text for card description.",
+                fileSize:".9mb",
+                close: false,
+                tag:{
+                    isOpen:true,tagTitle:"Upload",tagColor:"blue"
+                }
+                }
     ]
   return (
     <>
-    <div className="fixed top-0 left-0 z-[3] w-full h-full">
+    <div ref={ref} className="fixed top-0 left-0 z-[3] w-full h-full flex gap-5 flex-wrap p-5">
         {
-            data.map((item)=>{
-                <Cards data={item} />
-
-            }
+            data.map((item)=>(
+                    // eslint-disable-next-line react/jsx-key
+                    <Cards data={item} reference={ref}/>  
+                )
             )
         }
         </div>
